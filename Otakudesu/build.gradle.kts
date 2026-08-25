e7f73b46cec6e0e4239133a9be271ec304246d19
ByVoid <panggilajazeen@gmail.com>
2026-08-25 21:33:01 +0700

    Add files via upload

---
+++ Otakudesu/build.gradle.kts
@@ -0,0 +1,7 @@
+cloudstream {
+    description = "CloudStream provider Otakudesu"
+    authors = listOf("YOUR_NAME")
+    status = 0
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "id"
+}
