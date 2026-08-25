bafded480ff860034db28a7ba0ae2aba52a79dcf
ByVoid <panggilajazeen@gmail.com>
2026-08-25 21:24:00 +0700

    Add files via upload

---
+++ Donghuastream/src/build.gradle.kts
@@ -0,0 +1,7 @@
+cloudstream {
+    description = "CloudStream provider Donghuastream"
+    authors = listOf("YOUR_NAME")
+    status = 0
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "id"
+}
